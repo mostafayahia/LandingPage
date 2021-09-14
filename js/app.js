e40dedecf557fa7b04e242287fb07ba3ed9d2734
Yahia Hashem El-Tayeb <yahia.eltaib@yahoo.com>
2021-09-14 14:45:17 +0200

    feat: build the nav menu using js

--- js/app.js
+++ js/app.js
@@ -49,6 +49,30 @@
 */
 
 // Build menu 
+// create a fragment to hold menu items
+const fragment = document.createDocumentFragment();
+
+for (const section of document.querySelectorAll('section')) {
+    // get section name from the data-nav attribute in the section element
+    const sectionName = section.getAttribute("data-nav");
+    
+    // create a menu item
+    const menuItem = document.createElement('li');
+    
+    menuItem.classList = ["menu__link"];
+    
+    // set nav-data attribute for each of menu item to section id
+    menuItem.setAttribute("nav-data", section.id);
+
+    // set displayed text of the link to the section Name
+    menuItem.textContent = sectionName;
+
+    // append this item to the fragment
+    fragment.appendChild(menuItem);
+}
+
+// finally populate the fragment into navbar menu
+document.querySelector("#navbar__list").appendChild(fragment);
 
 // Scroll to section on link click
 
